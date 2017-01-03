e6ef9c643073a2263cde4950b64c9c47c6ae4a3a
Boris Quiroz <boris@insert-coin.org>
2017-01-03 00:20:53 -0300

    remove unused code

--- monitor.py
+++ monitor.py
@@ -13,19 +13,8 @@ class EventHandler(pyinotify.ProcessEvent):
         os.rename(event.pathname, new_name)
         subprocess.call(['scp', new_name, 'irc.zsh.io:/var/www/html/snap'])
 
-
 handler = EventHandler()
 notifier = pyinotify.Notifier(wm, handler)
 wdd = wm.add_watch('/tmp/screenshots', mask, rec=True)
 
 notifier.loop()
-#def main():
-#    wm = pyinotify.WatchManager()
-#    wm.add_watch('/tmp/screenshots', pyinotify.IN_CREATE, rec=True)
-#    notifier = pyinotify.Notifier(wm)
-#    message("New File!", "http://www.google.cl")
-#    os.system('scp %s irc.zsh.io:/var/www/html/snap' % filename)
-#    notifier.loop()
-#
-#def message(title, message):
-#    os.system('notify-send "'+title+'" "'+message+'"')
