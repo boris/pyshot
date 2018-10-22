11c4ff79ba53e7c319c3c868f1a0cd90258d323f
Boris Quiroz <boris@insert-coin.org>
2018-10-22 10:47:57 -0300

    remove uncomplete part

--- monitor.py
+++ monitor.py
@@ -14,10 +14,6 @@ class EventHandler(pyinotify.ProcessEvent):
         os.rename(event.pathname, new_name)
         subprocess.call(['scp', new_name, 'moby:apps/screenshots/imgs'])
 
-        # Needs to be fixed!
-        #url = "http://irc.zsh.io:8080/ss/" + new + ".png"
-        #clipboard.copy(url)
-
         os.remove(new_name)
 
 handler = EventHandler()
