3623e292a391dd27c69419d7110879d94a1492c5
Boris Quiroz <boris@insert-coin.org>
2020-05-08 12:00:14 -0400

    use local

--- monitor.py
+++ monitor.py
@@ -15,7 +15,8 @@ class EventHandler(pyinotify.ProcessEvent):
         # Needs to be fixed!
         #url = "http://irc.zsh.io:8080/ss/" + new + ".png"
         #clipboard.copy(url)
-        subprocess.call(['scp', new_name, 'moby:apps/screenshots/imgs'])
+        #subprocess.call(['scp','-i ~/.ssh/keys/id_rsa_pyshot', new_name, '192.168.1.12:~/docker/shots/images', 'shell=True'])
+        subprocess.call('scp -i ~/.ssh/keys/id_rsa_pyshot {} 192.168.1.12:~/docker/shots/images'.format(new_name), shell=True)
         notify = "notify-send 'https://imgs.zsh.io/{}.png'".format(new)
         os.system(notify)
         os.remove(new_name)
