440fe121c37c8090c48cd788e6dcd58fb0e8a1e6
Boris Quiroz <borisquiroz@pm.me>
2024-02-01 06:44:48 -0300

    Get screenshots from ~/Pictures and upload to S3

--- monitor.py
+++ monitor.py
@@ -7,19 +7,21 @@ import boto3
 wm = pyinotify.WatchManager()
 mask = pyinotify.IN_CREATE | pyinotify.IN_DELETE
 
-if not os.path.exists("/tmp/screenshots/"):
-    os.makedirs("/tmp/screenshots/")
+if not os.path.exists("~/Pictures/Screenshots/"):
+    os.makedirs("~/Pictures/Screenshots/")
 
 class EventHandler(pyinotify.ProcessEvent):
     def process_IN_CREATE(self, event):
+        print("Creating:", event.pathname)
         new = datetime.datetime.now().strftime("%s")
         new_name = event.path + "/" + new + ".png"
         os.rename(event.pathname, new_name)
-        subprocess.call('scp -i ~/.ssh/keys/id_rsa_pyshot {} jumphost-aws:~/shots'.format(new_name), shell=True)
+        subprocess.call(f"aws s3 cp {new_name} s3://borisquiroz.dev", shell=True)
+        print(f"Uploaded to S3: {new_name}")
         os.remove(new_name)
 
 handler = EventHandler()
 notifier = pyinotify.Notifier(wm, handler)
-wdd = wm.add_watch('/tmp/screenshots', mask, rec=True)
+wdd = wm.add_watch('~/Pictures/Screenshots', mask, rec=True)
 
 notifier.loop()
